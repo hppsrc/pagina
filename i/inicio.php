1fa08b930e6f17a797d2c533529a7c076d41fdf6
hppsrc <horahenaripo@gmail.com>
2024-11-21 15:02:36 -0500

    0.1.0
    - First commit

---
+++ i/inicio.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="hppsrc">
+    <meta name="title" content="Inicio">
+
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+
+    <title>Document</title>
+
+    <?php include 'php/page/meta.php'; ?>
+
+</head>
+
+<body>
+
+    <?php include 'php/page/modal.php'; ?>
+
+    <?php include 'php/page/header.php'; ?>
+
+    <main id="inicio">
+
+        <form>
+            <label> Buscar </label>
+            <input type="text" id="search" placeholder="Ingrese un id de video, id de estudiante, nombre o titulo">
+            <button>Buscar</button>
+        </form>
+
+        <br>
+
+        <form>
+            <label> ¿No sabes qué ver?</label>
+            <button>¡Sorprendeme!</button>
+        </form>
+
+        <br>
+
+        <div class="videosection">
+            <div class="row">
+                <h1>Videos destacados</h1>
+                <img src="res/icons/help-circle.svg" alt="" srcset="">
+            </div>
+
+            <div class="video-row">
+
+            <!-- ! Aqui se injecta el codigo -->
+
+                <div class="video-div">
+                    <div class="video"> <p>⭐5,00</p> </div>
+                    <p><b>Titulo</b></p>
+                    <p>Por:x xd</p>
+                </div>
+
+            </div>
+
+        </div>
+
+        <br>
+
+        <div class="videosection">
+            <div class="row">
+            <h1>Videos más recientes</h1>
+                <img src="res/icons/help-circle.svg" alt="" srcset="">
+            </div>
+
+            <div class="video-row">
+
+            <!-- ! Aqui se injecta el codigo -->
+                
+                <div class="video-div">
+                    <div class="video"></div>
+                    <p><b>Titulo</b></p>
+                    <p>Por:x xd</p>
+                </div>
+
+            </div>
+
+        </div>
+
+        <br>
+
+        <div class="videosection">
+            <div class="row">
+                <h1>Sugerencias para tí</h1>
+                <img src="res/icons/help-circle.svg" alt="" srcset="">
+            </div>
+
+            <div class="video-row">
+
+            <!-- ! Aqui se injecta el codigo -->
+                
+                <div class="video-div">
+                    <div class="video"></div>
+                    <p><b>Titulo</b></p>
+                    <p>Por:x xd</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </main>
+
+</body>
+</html>
---
+++ i/php/page/header.php
@@ -0,0 +1,18 @@
+<?php 
+    echo '
+        <header>
+            <div>
+                <img alt="Logo de CulturaTech" title="CulturaTech" src="res\icons\smile.svg" width="64px">
+                <div id="divcol">
+                    <h1> CulturaTech </h1>
+                    <p><small> Slogan de culturaTech</small></p>
+                </div>
+            </div>
+            <div id="divcol" style="text-align: right;">
+                <p id="username"><b> (Sin acceder) </b></p>
+                <p id="userrole"><b> Invitado </b></p>
+                <p id="actions"> Acciones </p>
+            </div>
+        </header>
+    ';
+?>
